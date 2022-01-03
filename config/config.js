53d038262f8d710002f0bed47a756e91a882b5ff
naman-agrawal89 <agrawal.naman89@gmail.com>
2022-01-03 16:28:07 +0530

    Removing files

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    WEB3_BSC_WSS_NODE_URL: "wss://bsc-ws-node.nariox.org:443",
-    WEB3_BSC_HTTP_NODE_URL: "https://bsc.getblock.io/mainnet/?api_key=b77b6aa6-4c08-43d6-8b87-f67a5d2b6ea1",
-    ROUTER_ADDRESS: "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F",
-    FACTORY_ADDRESS: "0xBCfCcbde45cE874adCB698cC183deBcF17952812"
-}
